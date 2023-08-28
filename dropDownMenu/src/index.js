1da502afccf1da88fad26ccaf934f3f4c41e5093
Andre <andre.potthast.ap@googlemail.com>
2023-08-28 11:28:44 +0200

    added js functionality for dropdown toggle visibility

--- dropDownMenu/src/index.js
+++ dropDownMenu/src/index.js
@@ -1,7 +1,21 @@
-// function component() {
-//   const element = document.createElement("div");
-//   element.innerHTML = "index.js works";
-//   return element;
-// }
+const dropdowns = document.querySelectorAll(".dropdownBtn");
 
-// document.body.appendChild(component());
+// close all open dropdown
+function closeDropdowns() {
+  const activeDropdowns = document.querySelectorAll(".dropdown.active");
+  activeDropdowns.forEach((dropdown) => {
+    dropdown.classList.remove("active");
+  });
+}
+
+function toggleDropdown(event) {
+  closeDropdowns();
+  event.preventDefault();
+  const dropdownList = this.querySelector(".dropdown");
+  dropdownList.classList.toggle("active");
+}
+dropdowns.forEach((dropdown) => {
+  dropdown.addEventListener("click", toggleDropdown);
+});
+
+// when window clicked close the dropdown
