e83e77b69e01927335aa472e648d3225422f4494
Andre <andre.potthast.ap@googlemail.com>
2023-09-24 11:05:17 +0200

    inital folder commit

---
+++ mobileMenu/dist/main.js
@@ -0,0 +1,25 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+const h1 = document.querySelector("h1");
+console.log(h1);
+const newDiv = document.createElement("div");
+const newContent = document.createTextNode("Hi there and greetings!");
+newDiv.appendChild(newContent);
+h1.appendChild(newDiv);
+
+const name = "James";
+
+const person = { first: name };
+
+console.log(person);
+
+const sayHelloLinting = (fName) => {
+  console.log(`Hello linting, ${fName}`);
+};
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBLGlCQUFpQjtBQUNqQjtBQUNBO0FBQ0E7QUFDQTtBQUNBLGdDQUFnQyxNQUFNO0FBQ3RDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vbW9iaWxlX21lbnUvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgaDEgPSBkb2N1bWVudC5xdWVyeVNlbGVjdG9yKFwiaDFcIik7XHJcbmNvbnNvbGUubG9nKGgxKTtcclxuY29uc3QgbmV3RGl2ID0gZG9jdW1lbnQuY3JlYXRlRWxlbWVudChcImRpdlwiKTtcclxuY29uc3QgbmV3Q29udGVudCA9IGRvY3VtZW50LmNyZWF0ZVRleHROb2RlKFwiSGkgdGhlcmUgYW5kIGdyZWV0aW5ncyFcIik7XHJcbm5ld0Rpdi5hcHBlbmRDaGlsZChuZXdDb250ZW50KTtcclxuaDEuYXBwZW5kQ2hpbGQobmV3RGl2KTtcclxuXHJcbmNvbnN0IG5hbWUgPSBcIkphbWVzXCI7XHJcblxyXG5jb25zdCBwZXJzb24gPSB7IGZpcnN0OiBuYW1lIH07XHJcblxyXG5jb25zb2xlLmxvZyhwZXJzb24pO1xyXG5cclxuY29uc3Qgc2F5SGVsbG9MaW50aW5nID0gKGZOYW1lKSA9PiB7XHJcbiAgY29uc29sZS5sb2coYEhlbGxvIGxpbnRpbmcsICR7Zk5hbWV9YCk7XHJcbn07XHJcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
