4618dbb5d0bb184f85b469448dfd151bb3d01219
Andre <andre.potthast.ap@googlemail.com>
2023-09-27 12:58:36 +0200

    finished it

--- mobileBottomMenu/index.js
+++ mobileBottomMenu/index.js
@@ -1,1 +1,19 @@
+const indicator = document.querySelector("#indicator");
+document.addEventListener("click", (e) => {
+  let anchor;
+  if (e.target.matches("a")) {
+    anchor = e.target;
+  } else {
+    anchor = e.target.closest("a");
+  }
+  if (anchor != null) {
+    const allAnchors = [...document.querySelectorAll("a")];
+    const index = allAnchors.indexOf(anchor);
+    indicator.style.setProperty("--position", index);
 
+    document.querySelectorAll("a").forEach((element) => {
+      element.classList.remove("active");
+    });
+    anchor.classList.add("active");
+  }
+});
